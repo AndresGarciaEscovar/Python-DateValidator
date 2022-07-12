d5be1f535a7ae2523336222d54e939982a72b6f5
andres <andrumen1@yahoo.com>
2022-07-11 21:36:58 -0500

    Finished writing the base function. Must improve it to handle long word cases.

--- date_validator/utilities/utilities_strings.py
+++ date_validator/utilities/utilities_strings.py
@@ -64,19 +64,79 @@ def to_length(string: str, characters: int = 80, indentation: int = 0) -> str:
 
     def set_length_0(line_0: str) -> str:
         """
-            Given a line, it
-        :param line_0:
-        :return:
+            Given a line, it properly formats the line such that it has the
+            given length.
+
+            :param line_0: The line to be formatted.
+
+            :return: The formatted line, at the given level of indentation.
         """
+
+        # Get the indent level.
+        indent_0 = "" if indentation == 0 else get_indent(indentation)
+
+        # The base string.
+        base_0 = [indent_0]
+
+        # The words.
+        tokens_0 = line_0.split(" ")
+
+        # Where the lines will be stored.
+        lines_0 = []
+
+        # For every token.
+        for i_0, word_0 in enumerate(tokens_0):
+
+            # Build the line.
+            base_0.append(word_0)
+
+            # Check for character length.
+            if len(" ".join(base_0)) > characters:
+                lines_0.append(" ".join(base_0[:-1]))
+                base_0 = [indent_0, base_0[-1]]
+
+        # Append the last line, if needed.
+        if len(base_0) > 1:
+            lines_0.append(" ".join(base_0))
+
+        return "\n".join(lines_0)
 
     # //////////////////////////////////////////////////////////////////////////
     # Implementation
     # //////////////////////////////////////////////////////////////////////////
 
     # Split the string at the new line characters.
-    lines = string.strip("\n")
+    lines = string.split("\n")
 
     # For every line.
-    for line in lines:
-        # Split at the spaces.
-        tokens = line.split(" ")
+    for i, line in enumerate(lines):
+        # Try to format the line.
+        lines[i] = set_length_0(line)
+
+    return "\n".join(lines)
+
+
+# ##############################################################################
+# TO DELETE
+# ##############################################################################
+
+
+if __name__ == "__main__":
+
+    # TODO: MAKE TESTS FOR THESE FUNCTIONS, THIS IS POSSIBLE.
+    # TODO: MUST BE REMOVED; FUNCTION MUST BE IMPROVED TO HANDLE LONGER WORDS.
+    text = (
+        "This is just an example of some text that I have written. For "
+        "example:\nWe shall start by taking this sentence and just writing "
+        "this, a story. This is really not meant to be anything sophisticated, "
+        "but just an example of a paragraph that will be turned into a block "
+        "of text with the proper length of each line. Of course, this is meant "
+        "to be a test, not the reallity.\nOh, yes, just for the fun of it, this"
+        " can contain words that are reaaaaaaaaaaaaaaaally long and could be, "
+        "potentially formatted in a better way."
+    )
+
+    # Set the string to the proper length.
+    text = to_length(text, characters=80, indentation=1)
+
+    print(text)
